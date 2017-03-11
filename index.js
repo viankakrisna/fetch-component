ca45dd873dd2344574b0a43d6394925ee3cad8ef
Ade Viankakrisna Fadlil <viankakrisna@gmail.com>
2017-03-11 23:46:07 +0700

    remove unused import

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-"use strict";
+'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
+Object.defineProperty(exports, '__esModule', {
+  value: true,
 });
 
 var _createClass = (function() {
@@ -10,7 +10,7 @@ var _createClass = (function() {
       var descriptor = props[i];
       descriptor.enumerable = descriptor.enumerable || false;
       descriptor.configurable = true;
-      if ("value" in descriptor) descriptor.writable = true;
+      if ('value' in descriptor) descriptor.writable = true;
       Object.defineProperty(target, descriptor.key, descriptor);
     }
   }
@@ -21,17 +21,13 @@ var _createClass = (function() {
   };
 })();
 
-var _react = require("react");
+var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
-var _isEqual = require("lodash/isEqual");
+var _isEqual = require('lodash/isEqual');
 
 var _isEqual2 = _interopRequireDefault(_isEqual);
-
-var _state = require("~/state");
-
-var _state2 = _interopRequireDefault(_state);
 
 function _interopRequireDefault(obj) {
   return obj && obj.__esModule ? obj : { default: obj };
@@ -39,7 +35,7 @@ function _interopRequireDefault(obj) {
 
 function _classCallCheck(instance, Constructor) {
   if (!(instance instanceof Constructor)) {
-    throw new TypeError("Cannot call a class as a function");
+    throw new TypeError('Cannot call a class as a function');
   }
 }
 
@@ -49,15 +45,15 @@ function _possibleConstructorReturn(self, call) {
       "this hasn't been initialised - super() hasn't been called"
     );
   }
-  return call && (typeof call === "object" || typeof call === "function")
+  return call && (typeof call === 'object' || typeof call === 'function')
     ? call
     : self;
 }
 
 function _inherits(subClass, superClass) {
-  if (typeof superClass !== "function" && superClass !== null) {
+  if (typeof superClass !== 'function' && superClass !== null) {
     throw new TypeError(
-      "Super expression must either be null or a function, not " +
+      'Super expression must either be null or a function, not ' +
         typeof superClass
     );
   }
@@ -66,13 +62,13 @@ function _inherits(subClass, superClass) {
       value: subClass,
       enumerable: false,
       writable: true,
-      configurable: true
-    }
+      configurable: true,
+    },
   });
   if (superClass)
     Object.setPrototypeOf
       ? Object.setPrototypeOf(subClass, superClass)
-      : subClass.__proto__ = superClass;
+      : (subClass.__proto__ = superClass);
 }
 
 var Fetch = (function(_React$Component) {
@@ -93,52 +89,51 @@ var Fetch = (function(_React$Component) {
       args[_key] = arguments[_key];
     }
 
-    return _ret = (_temp = (_this = _possibleConstructorReturn(
+    return (_ret = ((_temp = ((_this = _possibleConstructorReturn(
       this,
       (_ref = Fetch.__proto__ || Object.getPrototypeOf(Fetch)).call.apply(
         _ref,
         [this].concat(args)
       )
-    ), _this), _this.state = {
-      data: false
-    }, _temp), _possibleConstructorReturn(_this, _ret);
+    )), _this)), (_this.state = {
+      data: false,
+    }), _temp)), _possibleConstructorReturn(_this, _ret);
   }
 
   _createClass(Fetch, [
     {
-      key: "componentDidMount",
+      key: 'componentDidMount',
       value: function componentDidMount() {
         this.mounted = true;
         this.fetch(this.props);
-      }
+      },
     },
     {
-      key: "componentWillUnmount",
+      key: 'componentWillUnmount',
       value: function componentWillUnmount() {
         this.mounted = false;
-      }
+      },
     },
     {
-      key: "update",
+      key: 'update',
       value: function update() {
         if (this.mounted) {
           this.setState.apply(this, arguments);
         }
-      }
+      },
     },
     {
-      key: "fetch",
+      key: 'fetch',
       value: function fetch(props) {
         var _this2 = this;
 
         this.update({ data: getCache(props) });
         if (Array.isArray(props.url)) {
-          return Promise
-            .all(
-              props.url.map(function(url) {
-                return global.fetch(url, props.config);
-              })
-            )
+          return Promise.all(
+            props.url.map(function(url) {
+              return global.fetch(url, props.config);
+            })
+          )
             .then(function(results) {
               return Promise.all(
                 results.map(function(res) {
@@ -164,18 +159,18 @@ var Fetch = (function(_React$Component) {
           .catch(function(error) {
             _this2.update({ error: error });
           });
-      }
+      },
     },
     {
-      key: "componentWillReceiveProps",
+      key: 'componentWillReceiveProps',
       value: function componentWillReceiveProps(nextProps) {
         if (!(0, _isEqual2.default)(nextProps.url, this.props.url)) {
           this.fetch(nextProps);
         }
-      }
+      },
     },
     {
-      key: "render",
+      key: 'render',
       value: function render() {
         if (this.state.error && this.props.onError) {
           return this.props.onError(this.state.error);
@@ -186,8 +181,8 @@ var Fetch = (function(_React$Component) {
         }
 
         return null;
-      }
-    }
+      },
+    },
   ]);
 
   return Fetch;
