9b93869ccf1fc53f9667fa8a0739c40be7543b32
Ade Viankakrisna Fadlil <viankakrisna@gmail.com>
2017-04-22 17:25:32 +0700

    Add more robust api

--- src/index.js
+++ src/index.js
@@ -2,65 +2,90 @@ import React from 'react';
 import isEqual from 'lodash/isEqual';
 
 class Fetch extends React.Component {
-  state = {
-    data: false,
-  };
+	state = {
+		data: false,
+		loading: true,
+	};
 
-  componentDidMount() {
-    this.mounted = true;
-    this.fetch(this.props);
-  }
+	fetch = props => {
+		this.update({ data: false, loading: true, error: false });
+		if (!props.url) {
+			return new Promise(resolve => resolve());
+		}
+		if (Array.isArray(props.url)) {
+			return Promise.all(props.url.map(url => global.fetch(url, props.config)))
+				.then(results => Promise.all(results.map(res => res.json())))
+				.then(
+					data => {
+						this.update({ data, error: false, loading: false });
+					},
+					error => {
+						this.update({ data: false, error, loading: false });
+					}
+				);
+		}
+		return global
+			.fetch(props.url, props.config)
+			.then(res => {
+				if (res.status === 200) {
+					return res.json();
+				}
+				throw res;
+			})
+			.then(
+				data => {
+					this.update({ data, error: false, loading: false });
+				},
+				error => {
+					this.update({ error, data: false, loading: false });
+				}
+			);
+	};
 
-  componentWillUnmount() {
-    this.mounted = false;
-  }
+	componentDidMount() {
+		this.mounted = true;
+		this.fetch(this.props);
+	}
 
-  update(...args) {
-    if (this.mounted) {
-      this.setState(...args);
-    }
-  }
+	componentWillUnmount() {
+		this.mounted = false;
+	}
 
-  fetch(props) {
-    this.update({ data: false });
-    if (Array.isArray(props.url)) {
-      return Promise.all(props.url.map(url => global.fetch(url, props.config)))
-        .then(results => Promise.all(results.map(res => res.json())))
-        .then(data => {
-          this.update({ data, error: false });
-        })
-        .catch(error => {
-          this.update({ error });
-        });
-    }
-    return global
-      .fetch(props.url, props.config)
-      .then(res => res.json())
-      .then(data => {
-        this.update({ data, error: false });
-      })
-      .catch(error => {
-        this.update({ error });
-      });
-  }
+	update(...args) {
+		if (this.mounted) {
+			this.setState(...args);
+		}
+	}
 
-  componentWillReceiveProps(nextProps) {
-    if (!isEqual(nextProps.url, this.props.url)) {
-      this.fetch(nextProps);
-    }
-  }
+	componentWillReceiveProps(nextProps) {
+		if (!isEqual(nextProps.url, this.props.url)) {
+			this.fetch(nextProps);
+		}
+	}
 
-  render() {
-    if (this.state.error && this.props.onError) {
-      return this.props.onError(this.state.error);
-    }
+	render() {
+		try {
+			if (this.state.loading && this.props.onLoading) {
+				return this.props.onLoading();
+			}
 
-    if (this.props.children) {
-      return this.props.children(this.state.data);
-    }
+			if (this.state.error && this.props.onError) {
+				return this.props.onError(this.state.error, this.fetch);
+			}
 
-    return null;
-  }
+			if (this.state.data && this.props.children) {
+				return this.props.children(this.state.data);
+			}
+
+			if (this.state.data && this.props.onSuccess) {
+				return this.props.onSuccess(this.state.data);
+			}
+		} catch (e) {
+			return this.props.onError(e, this.fetch);
+		}
+
+		return null;
+	}
 }
 
 export default Fetch;
